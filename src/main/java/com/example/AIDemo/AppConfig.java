c98c7141e960c8773e3e3de0f9c358d112115344
Mohamed Fat-hy <mhmdfathy96@gmail.com>
2026-03-26 16:58:34 +0200

    feat: Migrate vector store implementation from pgvector to Redis.

--- src/main/java/com/example/AIDemo/AppConfig.java
+++ src/main/java/com/example/AIDemo/AppConfig.java
@@ -2,17 +2,23 @@ package com.example.AIDemo;
 
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+
+import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class AppConfig {
     @Bean
-    VectorStore vectorStore(JdbcTemplate jdbcTemplate,
-            @Qualifier("googleEmbeddingModel") EmbeddingModel embeddingModel) {
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel).build();
+    VectorStore vectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel) {
+        return RedisVectorStore.builder(jedisPooled, embeddingModel).indexName("product-index")
+                .initializeSchema(true)
+                .build();
+    }
+
+    @Bean
+    JedisPooled jedisPooled() {
+        return new JedisPooled("localhost", 6379);
     }
 }
