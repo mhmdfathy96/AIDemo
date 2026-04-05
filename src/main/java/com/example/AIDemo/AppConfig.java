e4e69f97ce5afcd4e8cfce69c3f3dbda4db66452
Mohamed Fat-hy <mhmdfathy96@gmail.com>
2026-04-05 12:16:17 +0200

    using OpenAI image generation and vision capabilities

--- src/main/java/com/example/AIDemo/AppConfig.java
+++ src/main/java/com/example/AIDemo/AppConfig.java
@@ -1,24 +1,7 @@
 package com.example.AIDemo;
 
-import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.redis.RedisVectorStore;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    VectorStore vectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel) {
-        return RedisVectorStore.builder(jedisPooled, embeddingModel).indexName("product-index")
-                .initializeSchema(true)
-                .build();
-    }
-
-    @Bean
-    JedisPooled jedisPooled() {
-        return new JedisPooled("localhost", 6379);
-    }
 }
